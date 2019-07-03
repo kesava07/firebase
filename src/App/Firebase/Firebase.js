a4e0088bf9705693450355bfaef2bf77934fca60
kesava07 <43296527+kesava07@users.noreply.github.com>
2019-07-03 02:40:08 -0700

    Update Firebase.js

--- src/App/Firebase/Firebase.js
+++ src/App/Firebase/Firebase.js
@@ -3,14 +3,6 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 
-const config = {
-    apiKey: "AIzaSyA3rW_KtCkk7zY_Gn-DPfW3ubYdjyJ7VIc",
-    authDomain: "complete-auth-99ccf.firebaseapp.com",
-    databaseURL: "https://complete-auth-99ccf.firebaseio.com",
-    projectId: "complete-auth-99ccf",
-    storageBucket: "complete-auth-99ccf.appspot.com",
-    messagingSenderId: "323709380193"
-  };
-  firebase.initializeApp(config);
+// your firebase configuration
 
   export default firebase;
